cdd4f34c5a318031cf9ca1dfd32a84b207d0cf17
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-03-07 20:02:41 -0300

    style: organization code style

--- src/infrastructure/order/repository/sequelize/order.repository.ts
+++ src/infrastructure/order/repository/sequelize/order.repository.ts
@@ -1,8 +1,8 @@
-import Order from "../../domain/checkout/entity/order";
-import OrderRepositoryInterface from "../../domain/checkout/repository/order-repository.interface";
-import OrderItemModel from "../db/sequelize/model/order-item.model";
-import OrderModel from "../db/sequelize/model/order.model";
-import OrderItem from "../../domain/checkout/entity/order_item";
+import Order from "../../../../domain/checkout/entity/order";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
+import OrderItemModel from "./order-item.model";
+import OrderModel from "./order.model";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 
 export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
